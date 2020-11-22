dbf7f49bf59fe654c79f60266b03362a611b31c7
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2020-11-22 20:42:28 +0200

    create content_history table on installation

--- contenteditor.php
+++ contenteditor.php
@@ -13,8 +13,27 @@ class contenteditor extends Module {
         $this->bootstrap = true;
     }
 
+    public function install()
+    {
+        $res = true;
+        $this->installTable();
+        return $res && parent::install();
+    }
 
     public function getContent() {
 
+    }
+
+    public function installTable()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS " . _DB_PREFIX_ . "content_history (" .
+            "`id_content_history` int(10) UNSIGNED NOT NULL AUTO_INCREMENT," .
+            "`id_employee` int(10) UNSIGNED NOT NULL," .
+            "`original_content` varchar(255) NOT NULL," .
+            "`new_content` varchar(255) DEFAULT NULL," .
+            "`added` datetime NOT NULL," .
+            "PRIMARY KEY (`id_content_history`)
+            ) DEFAULT CHARSET=utf8";
+        return Db::getInstance()->execute($sql);
     }
 }
