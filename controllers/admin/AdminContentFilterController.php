0a873de40e88d0ba7f59110670a007c2e3e6bf0b
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2020-11-29 21:30:00 +0200

    print table structure in collapsible blocks

--- controllers/admin/AdminContentFilterController.php
+++ controllers/admin/AdminContentFilterController.php
@@ -6,12 +6,22 @@ class AdminContentFilterController extends ModuleAdminController
     public function initContent()
     {
         $this->addCSS("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css");
+        $this->addJS("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.js");
         $tables = $this->module->getDbTables();
         $table_name_id = "Tables_in_" . _DB_NAME_;
-        $td = $this->module->getTableDescription("ps_cms_lang");
+        $table_descriptions = [];
+        foreach ($tables as $table)
+        {
+            $td = $this->module->getTableDescription($table[$table_name_id]);
+            foreach ($td as $t)
+            {
+                $table_descriptions[$table[$table_name_id]][$t['Field']] = $t['Type'];
+            }
+        }
         $this->context->smarty->assign([
             'tables' => $tables,
             'table_name_id' => $table_name_id,
+            'table_descriptions' => $table_descriptions,
         ]);
         $this->context->smarty->assign(array(
             'content' => $this->module->fetch("module:" . $this->module->name . "/views/templates/admin/list.tpl")
--- views/templates/admin/list.tpl
+++ views/templates/admin/list.tpl
@@ -2,9 +2,19 @@
     <form action="">
         <div class="row">
             {foreach $tables as $table}
+                {assign var='table_name' value=$table.$table_name_id}
+                {counter assign='id'}
                 <div class="col-lg-6 border border-success p-3">
                     <input type="checkbox">
-                    <p class="d-inline" >{$table.$table_name_id}</p>
+                    <a class="d-inline" data-toggle="collapse" href="#table-fields-{$id}" role="button">{$table_name}</a>
+                    <div id="table-fields-{$id}" class="collapse container col-lg-12">
+                        <div class="row">
+                            {foreach $table_descriptions.$table_name as $field => $type}
+                                <div class="col-lg-6"><input class="float-left" type="checkbox">{$field}</div>
+                                <div class="col-lg-6">{$type}</div>
+                            {/foreach}
+                        </div>
+                    </div>
                 </div>
             {/foreach}
         </div>
